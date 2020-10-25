f979fd028fc832c81f23adc42bb242c4b9a0703e
Saša Jurić <sasa.juric@gmail.com>
2020-10-25 23:21:51 +0100

    Add OsCmd server

--- lib/os_cmd.ex
+++ lib/os_cmd.ex
@@ -1,5 +1,91 @@
 defmodule OsCmd do
+  use GenServer
+
   defmodule Error do
     defexception [:message, :exit_code]
+  end
+
+  def start_link(args) when is_list(args), do: start_link({args, []})
+
+  def start_link({args, opts}),
+    do: GenServer.start_link(__MODULE__, {args, opts}, Keyword.take(opts, [:name]))
+
+  def stop(server, reason \\ :normal, timeout \\ :infinity),
+    do: GenServer.stop(server, reason, timeout)
+
+  @impl GenServer
+  def init({args, opts}) do
+    Process.flag(:trap_exit, true)
+
+    case open_port(args, opts) do
+      {:ok, port} -> {:ok, %{port: port, notify: Keyword.get(opts, :notify)}}
+      {:error, reason} -> {:stop, reason}
+    end
+  end
+
+  @impl GenServer
+  def handle_info({port, {:exit_status, exit_status}}, %{port: port} = state) do
+    if not is_nil(state.notify), do: send(state.notify, {self(), {:stopped, exit_status}})
+    {:stop, :normal, %{state | port: nil}}
+  end
+
+  def handle_info({port, {:data, message}}, %{port: port} = state) do
+    if not is_nil(state.notify), do: send(state.notify, {self(), :erlang.binary_to_term(message)})
+    {:noreply, state}
+  end
+
+  @impl GenServer
+  def terminate(_reason, %{port: nil}), do: :ok
+  def terminate(_reason, %{port: port}), do: stop_program(port)
+
+  defp open_port(args, opts) do
+    with {:ok, port_executable} <- port_executable() do
+      port =
+        Port.open({:spawn_executable, port_executable}, [
+          :stderr_to_stdout,
+          :exit_status,
+          :binary,
+          packet: 4,
+          args: args(opts) ++ args
+        ])
+
+      receive do
+        {^port, {:data, "started"}} ->
+          {:ok, port}
+
+        {^port, {:data, "not started " <> error}} ->
+          Port.close(port)
+          {:error, %Error{message: error}}
+      end
+    end
+  end
+
+  defp args(opts) do
+    Enum.flat_map(
+      opts,
+      fn
+        {:cd, dir} -> ["-dir", dir]
+        {:kill_cmd, cmd} -> ["-kill_cmd", cmd]
+        _other -> []
+      end
+    )
+  end
+
+  defp port_executable do
+    Application.app_dir(:ci, "priv")
+    |> Path.join("os_cmd*")
+    |> Path.wildcard()
+    |> case do
+      [executable] -> {:ok, executable}
+      _ -> {:error, %Error{message: "can't find os_cmd executable"}}
+    end
+  end
+
+  defp stop_program(port) do
+    Port.command(port, "stop")
+
+    receive do
+      {^port, {:exit_status, _exit_code}} -> :ok
+    end
   end
 end
---
+++ test/os_cmd_test.exs
@@ -0,0 +1,96 @@
+defmodule OsCmdTest do
+  use ExUnit.Case, async: true
+
+  test "stops the process after the command finishes" do
+    {:ok, pid} = OsCmd.start_link(~w/echo 1/)
+    mref = Process.monitor(pid)
+    assert_receive {:DOWN, ^mref, :process, ^pid, :normal}
+  end
+
+  test "sends notifications when configured" do
+    {:ok, pid} = OsCmd.start_link({~w/echo 1/, notify: self()})
+    assert_receive {^pid, {:output, "1\n"}}
+    assert_receive {^pid, {:stopped, 0}}
+  end
+
+  test "returns the correct exit code" do
+    {:ok, pid} = OsCmd.start_link({["bash", "-c", ~s/exit 42/], notify: self()})
+    assert_receive {^pid, {:stopped, 42}}
+  end
+
+  test "sets the correct folder" do
+    {:ok, pid} = OsCmd.start_link({~w/pwd/, notify: self(), cd: "test"})
+    assert_receive {^pid, {:output, output}}
+    assert output |> String.trim_trailing() |> Path.relative_to_cwd() == "test"
+  end
+
+  test "executes the specified kill command" do
+    {:ok, pid1} = OsCmd.start_link(~w/sleep infinity/)
+    Process.monitor(pid1)
+
+    {:ok, pid2} = OsCmd.start_link({~w/sleep infinity/, kill_cmd: "killall sleep"})
+    Process.monitor(pid2)
+
+    OsCmd.stop(pid2)
+    assert_receive {:DOWN, _, :process, ^pid1, :normal}
+    assert_receive {:DOWN, _, :process, ^pid2, :normal}
+  end
+
+  test "terminates the program on stop" do
+    on_exit(fn -> File.rm("test.txt") end)
+
+    {:ok, pid} =
+      OsCmd.start_link([
+        "bash",
+        "-c",
+        """
+        while true; do
+          echo 1 >> test.txt
+          sleep 0.001
+        done
+        """
+      ])
+
+    Process.sleep(100)
+    assert OsCmd.stop(pid) == :ok
+
+    contents = File.read!("test.txt")
+    Process.sleep(100)
+    assert File.read!("test.txt") == contents
+  end
+
+  test "terminates the program when the owner process dies" do
+    on_exit(fn -> File.rm("test.txt") end)
+
+    test_pid = self()
+
+    {:ok, owner_pid} =
+      Agent.start_link(fn ->
+        {:ok, cmd_pid} =
+          OsCmd.start_link([
+            "bash",
+            "-c",
+            """
+            while true; do
+              echo 1 >> test.txt
+              sleep 0.001
+            done
+            """
+          ])
+
+        send(test_pid, cmd_pid)
+      end)
+
+    assert_receive cmd_pid
+    Process.monitor(cmd_pid)
+
+    Process.sleep(100)
+    Agent.stop(owner_pid)
+
+    assert_receive {:DOWN, _mref, :process, ^cmd_pid, _}
+
+    contents = File.read!("test.txt")
+    Process.sleep(100)
+    assert File.read!("test.txt") == contents
+  end
+end
