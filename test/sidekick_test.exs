8820d8c9a5ea0bd27710da45f10a4d5f5495fdb7
Hans Pagh <hanspagh@gmail.com>
2021-04-07 13:32:34 +0200

    extend tests

--- test/sidekick_test.exs
+++ test/sidekick_test.exs
@@ -2,6 +2,19 @@ defmodule SidekickTest do
   use ExUnit.Case, async: false
 
   test "starts a remote node" do
-    assert Sidekick.start([]) == :ok
+    assert Sidekick.start(:test, []) == :ok
+
+    # Sidekick node should exist
+    assert Node.list() == [:"test@127.0.0.1"]
+
+    # Supervisor should be started on the Node
+    pid = GenServer.whereis({Sidekick.Supervisor, :"test@127.0.0.1"})
+    assert pid != nil
+
+    # Shutting down the supervisor should close down the node
+    Node.monitor(:"test@127.0.0.1", true)
+    Supervisor.stop(pid)
+    assert_receive {:nodedown, :"test@127.0.0.1"}, 2000
+    assert Node.list() == []
   end
 end
