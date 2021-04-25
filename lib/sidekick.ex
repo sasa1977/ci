13a4a0dfff456e4b1593af4e2ab3672a07f5b842
Saša Jurić <sasa.juric@gmail.com>
2021-04-25 22:25:44 +0200

    Refactor port implementation

--- lib/sidekick.ex
+++ lib/sidekick.ex
@@ -2,12 +2,7 @@ defmodule Sidekick do
   @spec start(atom, [Parent.child_spec()]) :: :ok | {:error, :already_started | :boot_error}
   def start(node_name, children) do
     ensure_distributed!()
-
-    node = :"#{node_name}@#{hostname()}"
-
-    if Node.ping(node) == :pong,
-      do: {:error, :already_started},
-      else: start_node(node, children)
+    Sidekick.Port.start_link(node_name, children)
   end
 
   defp ensure_distributed! do
@@ -38,51 +33,5 @@ defmodule Sidekick do
     {:ok, _} = Sidekick.Supervisor.start(caller, children)
 
     send(caller, {__MODULE__, :initialized})
-  end
-
-  defp hostname do
-    [_name, hostname] = String.split("#{node()}", "@", parts: 2)
-    hostname
-  end
-
-  defp start_node(sidekick_node, children) do
-    {:ok, port_pid} = Sidekick.Port.start_link(self())
-    command = start_node_command(sidekick_node, port_pid, children)
-    Sidekick.Port.open(port_pid, command)
-
-    receive do
-      {__MODULE__, :initialized} -> {:ok, port_pid}
-      :port_closed -> {:error, :boot_error}
-    end
-  end
-
-  defp start_node_command(sidekick_node, parent_pid, children) do
-    {:ok, command} = :init.get_argument(:progname)
-
-    name_arg =
-      case :net_kernel.longnames() do
-        true -> "-name #{sidekick_node}"
-        false -> "-sname #{sidekick_node}"
-        _ -> raise "not in distributed mode"
-      end
-
-    base_args = "-noinput #{name_arg}"
-
-    priv_dir = :code.priv_dir(:ci)
-    boot_file_args = "-boot #{priv_dir}/node"
-
-    cookie = Node.get_cookie()
-    cookie_arg = "-setcookie #{cookie}"
-
-    paths_args = :code.get_path() |> Enum.map(&"-pa #{&1}") |> Enum.join(" ")
-
-    encoded_arg =
-      {parent_pid, children} |> :erlang.term_to_binary() |> Base.encode32(padding: false)
-
-    command_args = "-run Elixir.Sidekick sidekick_init #{encoded_arg}"
-
-    args = "#{base_args} #{boot_file_args} #{cookie_arg} #{paths_args} #{command_args}"
-
-    "#{command} #{args}"
   end
 end
--- lib/sidekick/port.ex
+++ lib/sidekick/port.ex
@@ -4,8 +4,12 @@ defmodule Sidekick.Port do
   use GenServer
   require Logger
 
-  def start_link(caller) do
-    GenServer.start_link(__MODULE__, [caller])
+  def start_link(node_name, children) do
+    node = :"#{node_name}@#{hostname()}"
+
+    if Node.ping(node) == :pong,
+      do: {:error, :already_started},
+      else: GenServer.start_link(__MODULE__, {node, children})
   end
 
   def open(pid, command) do
@@ -13,30 +17,55 @@ defmodule Sidekick.Port do
   end
 
   @impl GenServer
-  def init([caller]) do
-    {:ok, {caller}}
-  end
-
-  @impl GenServer
-  def handle_cast({:open, command}, {caller}) do
+  def init({node, children}) do
+    command = start_node_command(node, children)
     port = Port.open({:spawn, command}, [:stream, :exit_status])
-    {:noreply, {port, caller}}
+
+    receive do
+      {Sidekick, :initialized} -> {:ok, port}
+      {^port, {:exit_status, _status}} -> {:stop, :boot_error}
+    end
   end
 
   @impl GenServer
-  def handle_info({port, {:data, data}}, {port, caller}) do
+  def handle_info({port, {:data, data}}, port) do
     Logger.debug("Port got message #{inspect(data)}")
-    {:noreply, {port, caller}}
+    {:noreply, port}
   end
 
-  def handle_info({module, :initialized}, {port, caller}) do
-    send(caller, {module, :initialized})
-    {:noreply, {port, caller}}
-  end
-
-  def handle_info({port, {:exit_status, reason}}, {port, caller}) do
+  def handle_info({port, {:exit_status, reason}}, port) do
     Logger.debug("Port closed with reason #{reason}")
-    send(caller, :port_closed)
-    {:stop, :normal, {port, caller}}
+    {:stop, :normal, port}
+  end
+
+  defp hostname do
+    [_name, hostname] = String.split("#{node()}", "@", parts: 2)
+    hostname
+  end
+
+  defp start_node_command(sidekick_node, children) do
+    {:ok, command} = :init.get_argument(:progname)
+
+    name_arg =
+      case :net_kernel.longnames() do
+        true -> "-name #{sidekick_node}"
+        false -> "-sname #{sidekick_node}"
+        _ -> raise "not in distributed mode"
+      end
+
+    base_args = "-noinput #{name_arg}"
+
+    priv_dir = :code.priv_dir(:ci)
+    boot_file_args = "-boot #{priv_dir}/node"
+
+    cookie = Node.get_cookie()
+    cookie_arg = "-setcookie #{cookie}"
+
+    paths_args = :code.get_path() |> Enum.map(&"-pa #{&1}") |> Enum.join(" ")
+
+    encoded_arg = {self(), children} |> :erlang.term_to_binary() |> Base.encode32(padding: false)
+    command_args = "-run Elixir.Sidekick sidekick_init #{encoded_arg}"
+
+    "#{command} #{base_args} #{boot_file_args} #{cookie_arg} #{paths_args} #{command_args}"
   end
 end
